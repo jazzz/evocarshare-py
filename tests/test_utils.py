360da62b68a44d7fb30d1905770ae5a7141b5175
Jazz Turner-Baggs <473256+jazzz@users.noreply.github.com>
2024-11-22 19:01:53 -0800

    simplify envar loading

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,14 +1,17 @@
-from dotenv import dotenv_values
+import os
+
+from dotenv import load_dotenv
 
 from evocarshare import CredentialBundle
+
+load_dotenv(".env")
 
 
 def credentials_from_env() -> CredentialBundle:
     """Creates a CredentialBundle with secrets loaded from Environment"""
-    config = dotenv_values(".env")
 
-    api_key = config["EVOAPI_KEY"] or ""
-    client_id = config["EVOAPI_CLIENTID"] or ""
-    client_secret = config["EVOAPI_CLIENTSECRET"] or ""
+    api_key = os.environ.get("EVOAPI_KEY") or ""
+    client_id = os.environ.get("EVOAPI_CLIENTID") or ""
+    client_secret = os.environ.get("EVOAPI_CLIENTSECRET") or ""
 
     return CredentialBundle(api_key, client_id, client_secret)
